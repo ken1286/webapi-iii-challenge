ec371c9eb64a83a1387cbc9d698e8b1869af4c67
ken1286 <kindle1286@gmail.com>
2019-05-29 00:14:05 -0700

    basic api built

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code away!
+const server = require('./server.js');
+
+
+server.listen(8000, () => console.log('API Running on port 8000'));
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,20 @@
-const express = 'express';
-
+const express = require('express');
+const db = require('./userDb.js');
 const router = express.Router();
 
 router.post('/', (req, res) => {
+  const newUser = req.body;
+  if(!newUser.name) {
+    res.status(400).json({ error: 'Please provide a name for the user.'});
+  }
 
+  db.insert(newUser)
+    .then( user => {
+      res.status(200).json({ user });
+    })
+    .catch( err => {
+      res.status(500).json({ error: "There was an error while saving the user to the database" })
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -12,28 +23,84 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
 
+  db.get()
+    .then( users => {
+      res.status(200).json({ users });
+    })
+    .catch( err => {
+      res.status(500).json({ error: "The users information could not be retrieved." })
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
+  const { id } = req.params;
 
+  db.getById(id)
+    .then( user => {
+        res.status(200).json({ user });
+      })
+    .catch( err => {
+      res.status(500).json({ error: "The user information could not be retrieved." });
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
+  const { id } = req.params;
 
+  db.getUserPosts(id)
+    .then( posts => {
+      res.status(200).json({ posts });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The user posts could not be retrieved'});
+    })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
+  const { id } = req.params;
 
+  db.remove(id)
+    .then( user => {
+      res.status(204).json({ user })
+    })
+    .catch( err => {
+       res.status(500).json({ message: 'error' });
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
+  const updatedUser = req.body;
+  const { id } = req.params;
+  if(!updatedUser.name) {
+    res.status(400).json({ error: 'Please provide a name for the user.'});
+  }
 
+  db.update(id, updatedUser)
+    .then( user => {
+       res.status(204).json({ user })
+    })
+    .catch( err => {
+       res.status(500).json({ message: 'error' });
+     })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+  const { id } = req.params;
 
+  db.getById(id)
+    .then( user => {
+      if(user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(404).json({ message: "User not found: Invalid ID." })
+      }
+    })
+    .catch( err => {
+      res.status(500).json({ message: 'Failed to process request.'})
+    })
 };
 
 function validateUser(req, res, next) {

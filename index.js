912720338f7b4510293a3dbc91d483982a6ac441
ken1286 <kindle1286@gmail.com>
2019-05-30 12:41:47 -0700

    updates for heroku deployment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // code away!
 const server = require('./server.js');
 
-
-server.listen(8000, () => console.log('API Running on port 8000'));
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`API Running on port ${port}`));
